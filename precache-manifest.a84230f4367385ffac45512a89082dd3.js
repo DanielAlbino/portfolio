dad5f1a15cf4e3c743e423296026a9b4e71ebf7d
Daniel Albino <danielalbino@localhost.localdomain>
2020-01-01 12:33:47 +0000

    Updates

---
+++ precache-manifest.a84230f4367385ffac45512a89082dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0828d1dead06a9a16e2f0bb6c7c9454e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8ebcedbdf0842501ada9",
+    "url": "/portfolio/static/css/main.180ad066.chunk.css"
+  },
+  {
+    "revision": "1fab80910d8a9c62055f",
+    "url": "/portfolio/static/js/2.e9a1a0da.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.e9a1a0da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ebcedbdf0842501ada9",
+    "url": "/portfolio/static/js/main.7a0e547f.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.777f90c3d4676f5bd4d6639a91914845.js"
+  "/portfolio/precache-manifest.a84230f4367385ffac45512a89082dd3.js"
 );
 
 self.addEventListener('message', (event) => {
