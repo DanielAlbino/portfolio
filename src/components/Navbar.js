5150da79130021d9326f567929122868e6652d0d
Daniel Albino <45365463+DanielAlbino@users.noreply.github.com>
2020-01-02 09:06:45 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
         var github = 'https://github.com/DanielAlbino'
         return (
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <Link class="navbar-brand" to="/"> DANIEL ALBINO</Link>
+                <h1 class="navbar-brand"> DANIEL ALBINO</h1>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
